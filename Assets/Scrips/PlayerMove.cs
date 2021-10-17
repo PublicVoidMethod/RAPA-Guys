ab2cc740b1fe79f9dafd5644cf6293ee477e32f9
PublicVoidMethod <hsp1603@naver.com>
2021-10-18 06:30:01 +0900

    플레이어 중력 및 점프 구현중

--- Assets/Scrips/PlayerMove.cs
+++ Assets/Scrips/PlayerMove.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float playerSpeed = 5.0f;
+    public float gravity = -20.0f;
+    public float jumpPower = 15.0f;
+
+    float yVelocity = 0;
+    int jumpCount = 1;
 
     CharacterController cc;
 
@@ -29,12 +34,32 @@ public class PlayerMove : MonoBehaviour
         Vector3 dir = new Vector3(h, 0, v);
         dir.Normalize();
 
+        // 방향 벡터를 카메라의 방향을 기준으로 재계산 한다.
+        dir = Camera.main.transform.TransformDirection(dir);
+
+        PlayerJump();
+
+        // 중력 값을 누적한다.
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+
         // 움직이게 한다.
         cc.Move(dir * playerSpeed * Time.deltaTime);
     }
 
     void PlayerJump()
     {
+        if(cc.collisionFlags == CollisionFlags.Below)
+        {
+            jumpCount = 1;
+        }
 
+        // 점프를 하기 위해 스페이스바의 입력을 받는다면
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            // 위쪽 방향으로 점프력을 추가한다.
+            yVelocity = jumpPower;
+            jumpCount--;
+        }
     }
 }
