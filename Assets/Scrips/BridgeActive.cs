55b4dc4a22c5a70f064c0295422e7ea4a98ec7d2
PublicVoidMethod <hsp1603@naver.com>
2021-10-25 11:23:14 +0900

    2번쨰 장애물 작업중, 다리 작업중

--- Assets/Scrips/BridgeActive.cs
+++ Assets/Scrips/BridgeActive.cs
@@ -12,6 +12,21 @@ public class BridgeActive : MonoBehaviour
     {
         go = transform.GetChild(0).gameObject;
         StartCoroutine(BridgeONOFF(true));
+        //go = transform.GetChild(1).gameObject;
+        //StartCoroutine(BridgeONOFF(false));
+        //go = transform.GetChild(2).gameObject;
+        //StartCoroutine(BridgeONOFF(true));
+        //go = transform.GetChild(3).gameObject;
+        //StartCoroutine(BridgeONOFF(false));
+
+        //if(go == transform.GetChild(0).gameObject && go == transform.GetChild(1))
+        //{
+        //    StartCoroutine(BridgeONOFF(true));
+        //}
+        //else
+        //{
+        //    StartCoroutine(BridgeONOFF(false));
+        //}
     }
 
     IEnumerator BridgeONOFF(bool startValue)
